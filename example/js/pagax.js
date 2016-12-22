5d89499b06e6a150391415198268cfb97afe0df5
Achin Sood <achins87@gmail.com>
2016-12-22 12:37:20 +0530

    fixed multiple array values

--- example/js/pagax.js
+++ example/js/pagax.js
@@ -414,9 +414,7 @@ pagax_modules.forms = pagax_modules.forms || {
                             if(!$.isArray(arr[element_name])) {
                                 arr[element_name] = [];
                             }
-                            if(arr[element_name].indexOf($(this).val()) < 0) {
-                                arr[element_name].push($(this).val());
-                            }
+                            arr[element_name].push($(this).val());
                         }
 
                     } else {
